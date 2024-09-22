9d2d72b2a24473ca05ec7a764e7f20f0ec8140c0
andirkh <andirkh@icloud.com>
2024-09-22 17:37:25 +0700

    feat: add feature attribute to hide element when loading

--- brifter.js
+++ brifter.js
@@ -1,26 +1,19 @@
 const LEVER_ATTR = 'data-shift-lever';
-const TARGET_ATTR = 'data-target';
+const TARGET_ATTR = 'data-shift-target-id';
+
+const LOADING_ATTR = 'data-shift-loading-id';
+const HIDE_ONLOAD_ATTR = 'data-loading-hide-id';
+const RETAIN_INPUT_ATTR = 'data-retain-input';
+
 const LISTENED_ATTR = 'data-connected';
 
-// levers:
+// Levers variant:
 const LEVER_SWAP = 'swap';
 const LEVER_APPEND = 'append';
 const LEVER_PREPEND = 'prepend';
 
-// feedbacks:
+// Feedback from response header :
 const FEEDBACK_HEADER = 'X-Feedback-Message';
-
-const lockInputs = (elements, state) => {
-  elements.forEach(item => {
-    item.disabled = state;
-  });
-};
-
-const clearInputs = (elements) => {
-  elements.forEach(item => {
-    item.value = '';
-  })
-}
 
 const handleResponseLogic = (logic, targetElement, responseHtml) => {
   if (!targetElement) return;
@@ -36,9 +29,53 @@ const handleResponseLogic = (logic, targetElement, responseHtml) => {
       targetElement.insertAdjacentHTML('afterbegin', responseHtml);
       break;
     default:
-      console.warn('Unknown logic operation');
+      return;
   }
 };
+
+const lockInputs = (elements, state) => {
+  elements.forEach(item => {
+    item.disabled = state;
+  });
+};
+
+const clearInputs = (elements) => {
+  elements.forEach(item => {
+    item.value = '';
+  })
+}
+
+const showSnackbar = (message) => {
+  const snackbar = document.getElementById('snackbar');
+  snackbar.textContent = message;
+  snackbar.className = 'show';
+
+  setTimeout(() => {
+    snackbar.className = snackbar.className.replace('show', '');
+  }, 3000);
+}
+
+const showLoading = (loadingId, state) => {
+  const loadingElement = document.querySelector(loadingId);
+  if (!loadingElement) return;
+
+  if (state === true) {
+    loadingElement.className = `${loadingElement.className} show`
+  } else if (state === false) {
+    loadingElement.className = loadingElement.className.replace('show', '').trim();
+  }
+}
+
+const hideElementWhenLoading = (elementToHideId, state) => {
+  const hideElement = document.querySelector(elementToHideId)
+
+  if (!hideElement) return;
+  if (state === true) {
+    hideElement.className = `${hideElement.className} hidden`
+  } else if (state === false) {
+    hideElement.className = hideElement.className.replace('hidden', '').trim();
+  }
+}
 
 const buildUrlWithParams = (url, formData) => {
   const urlParams = new URLSearchParams();
@@ -61,48 +98,63 @@ const baseService = async (url, method, body) => {
   return await fetch(url, options);
 };
 
-// Function to handle GET and POST requests
-const handleShifting = async (form) => {
+const handleProgress = (loadingId, state, elementToHideId) => {
+  loadingId && showLoading(loadingId, state);
+  elementToHideId && hideElementWhenLoading(elementToHideId, state);
+}
+
+const handleCablePull = async (form) => {
   const formData = new FormData(form);
   const url = form.getAttribute('action');
   const method = form.getAttribute('method')?.toUpperCase() || 'POST';
   const logic = form.getAttribute(LEVER_ATTR);
-  const targetSelector = form.getAttribute(TARGET_ATTR);
-  const targetElement = document.querySelector(targetSelector);
+  const retainInput = form.getAttribute(RETAIN_INPUT_ATTR);
+  const targetId = form.getAttribute(TARGET_ATTR);
+
+  const targetElement = document.querySelector(targetId);
+
+  let fetchUrl = url;
+  let requestBody = null;
+
+  if (method === 'GET') {
+    fetchUrl = buildUrlWithParams(url, formData);
+  } else {
+    const urlEncodedData = new URLSearchParams(formData);
+    requestBody = urlEncodedData.toString();
+  }
+
+  const response = await baseService(fetchUrl, method, requestBody);
+
+  handleProgress(loadingId, false, elementToHideId)
+
+  if (response.ok) {
+    const responseHtml = await response.text();
+    handleResponseLogic(logic, targetElement, responseHtml);
+
+    const feedbackMessage = response.headers.get(FEEDBACK_HEADER);
+    feedbackMessage && showSnackbar(feedbackMessage);
+
+    !retainInput && clearInputs(inputsAndButtons);
+
+    // Re-attach form listeners:
+    connectToDrivetrain();
+  } else {
+    const errorMessage = `Server response error: ${response.statusText}`
+    handleProgress(loadingId, false, elementToHideId)
+    showSnackbar(errorMessage);
+  }
+}
+
+const handleShifting = async (form) => {
   const inputsAndButtons = form.querySelectorAll('input, button');
+  const loadingId = form.getAttribute(LOADING_ATTR);
+  const elementToHideId = form.getAttribute(HIDE_ONLOAD_ATTR);
 
   lockInputs(inputsAndButtons, true);
+  handleProgress(loadingId, true, elementToHideId)
 
   try {
-    let fetchUrl = url;
-    let requestBody = null;
-
-    if (method === 'GET') {
-      fetchUrl = buildUrlWithParams(url, formData);
-    } else {
-      const urlEncodedData = new URLSearchParams(formData);
-      requestBody = urlEncodedData.toString();
-    }
-
-    const response = await baseService(fetchUrl, method, requestBody);
-
-    if (response.ok) {
-      const responseHtml = await response.text();
-      handleResponseLogic(logic, targetElement, responseHtml);
-
-      const feedbackMessage = response.headers.get(FEEDBACK_HEADER);
-
-      if (feedbackMessage) {
-        showSnackbar(feedbackMessage);
-      }
-      clearInputs(inputsAndButtons);
-
-      // Re-attach form listeners:
-      connectToDrivetrain();
-    } else {
-      const errorMessage = `Server response error: ${response.statusText}`
-      showSnackbar(errorMessage);
-    }
+    await handleCablePull(form);
   } catch (error) {
     console.error('Fetch error:', error);
     showSnackbar("an error has occured!")
@@ -110,16 +162,6 @@ const handleShifting = async (form) => {
     lockInputs(inputsAndButtons, false);
   }
 };
-
-const showSnackbar = (message) => {
-  const snackbar = document.getElementById('snackbar');
-  snackbar.textContent = message;
-  snackbar.className = 'show';
-
-  setTimeout(() => {
-    snackbar.className = snackbar.className.replace('show', '');
-  }, 3000);
-}
 
 const connectToDrivetrain = () => {
   const forms = document.querySelectorAll('form');
